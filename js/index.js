24539a0843ee0ea85b0af41c6604b77c4174e093
ArSrNa <arsrna.cn@outlook.com>
2022-07-11 17:15:00 +0800

    颜色修改

--- js/index.js
+++ js/index.js
@@ -12,47 +12,47 @@ var activities = [{
 }, {
 	"title": "Happy Birthday ~~TDP周年庆活动来袭~~",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/2032462"
 }, {
 	"title": "腾云先锋第四期招募令已出，TDP男孩女孩们 速来~~",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/2014209"
 }, {
 	"title": "【开箱吧腾讯云——第二期对象存储COS】有奖互动来啦！",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/2007510"
 }, {
 	"title": "【开箱吧腾讯云——第一期lighthouse】有奖互动来啦！",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/1993405"
 }, {
 	"title": "#我与腾讯云不得不说的故事#有奖互动",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/1963773"
 }, {
 	"title": "TDP·腾讯云产品“用户实践”征文活动",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/1948608"
 }, {
 	"title": "TDP年末盛典活动，三大篇章共赴新春！",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "https://cloud.tencent.com/developer/article/1934619"
 }, {
 	"title": "双旦“协旧迎新”，组队赢取好礼！",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "/20211223/"
 }, {
 	"title": "腾讯云智能客服智囊团活动·第一期",
 	"status": "已结束",
-	"statusIcon": "success",
+	"statusIcon": "secondary",
 	"link": "/top.php"
 }];
 
